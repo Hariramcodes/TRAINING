89cd1dab7f7b013351b67d4e78c92dea738f7d20
Hariramcodes <94062254+Hariramcodes@users.noreply.github.com>
2023-02-21 18:47:27 +0530

    Add files via upload

---
+++ react-area-calculation/src/App.js
@@ -0,0 +1,42 @@
+import logo from './logo.svg';
+import './App.css';
+import {Circle,Triangle,Square,Squareper,Rombus} from './Function.js';
+import {Circle1,Triangle1,Square1,Squareper1,Rombus1} from './Myclass.js';
+
+function App() {
+  return (
+    <div className="App">
+      <div class="fun">
+      <h1>////USING FUNCTION////</h1>
+       <h1>Area of Circle</h1> 
+      <Circle />
+      <h1>Area of Triangle
+      </h1>
+<Triangle />
+<h1>Area of Square</h1>
+<Square />
+<h1>Perimeter of Square</h1>
+ <Squareper />
+ <h1>Area of Rombus</h1>
+ <Rombus /> 
+ </div>
+ <div class="cls">
+  <h1>////USING CLASS////</h1>
+<h1>Area of Circle</h1>
+<Circle1 />
+      <h1>Area of Triangle
+      </h1>
+<Triangle1 />
+<h1>Area of Square</h1>
+<Square1 />
+<h1>Perimeter of Square</h1>
+ <Squareper1 />
+ <h1>Area of Rombus</h1>
+ <Rombus1 /></div>
+
+
+    </div>
+  );
+}
+
+export default App;
